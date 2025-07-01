e6a28885d56bd823083e066639f83b812d77b59b
Caua Mattos <caua.h4k@gmail.com>
2025-06-30 22:01:11 -0300

    feat(page load):  add validations and the fetch method from PageLoad

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,13 +1,25 @@
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ url }) => {
+export const load: PageLoad = async ({ url, fetch }) => {
     const urlName = url.searchParams.get('name')?.trim()
+
+    if (urlName === null || urlName === undefined || urlName?.length <= 0) {
+        return {message: "Nenhum nome inserido"}
+    }
 
     let res = await fetch(`https://api.agify.io?name=${urlName}`, {
         method: "GET"
     })
 
+    if (!res.ok) {
+        return {
+            message: "Ocorreu um erro: " + res.statusText
+        }
+    }
+
     let data = await res.json()
+
+    console.log(data)
 
     return {
         data
