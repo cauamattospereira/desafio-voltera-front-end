dd5b847b1a530ff02c0c04dc0d807cd544cb9a44
Caua Mattos <caua.h4k@gmail.com>
2025-06-30 22:02:58 -0300

    feat(home page): create an HTML form, onSubmit and goto functions. API requests being realized with correct input

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,29 @@
 <script lang="ts">
+	import { goto } from "$app/navigation";
 	import type { PageProps } from "./$types";
 
     let { data }: PageProps = $props();
 
-    console.log(data)
 
-    function onSubmit() {
+    function onSubmit(event: SubmitEvent) {
+        const form = event.currentTarget as HTMLFormElement;
+        const formData = new FormData(form);
+
+        const name = formData.get('name')?.toString().trim()
+
+        if (name) {
+            goto(`/?name=${encodeURIComponent(name)}`)
+        }
+
     }
 </script>
 
-<h1>Hello {data.data.name ?? name}</h1>
+<h1>Olá {data.data?.name ?? "visitante!"}</h1>
+<p>{data.data?.age ?? "Não há uma estimativa de idade para esse nome"}</p>
 
 
 
-<form onsubmit={onSubmit}>
-    <input type="text">
+<form onsubmit={(data) => onSubmit(data)}>
+    <input type="text" name="name">
     <button type="submit">Enviar</button>
 </form>
