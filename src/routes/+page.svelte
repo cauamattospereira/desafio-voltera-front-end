0264abeaa1204d324431c57908f9554f9f26b458
Caua Mattos <caua.h4k@gmail.com>
2025-07-04 13:51:59 -0300

    feat(response ui): mobile responsive UI added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,10 +15,11 @@
 </script>
 
 
-<div class="main" style="padding: 1rem; position: relative; overflow: hidden;">
+<div class="main" style="padding: 1rem; position: relative; overflow-x: hidden;">
     <!-- navbar -->
     <nav style="
   position: fixed;
+  display: block;
   top: 0;
   width: 100%;
   display: flex;
@@ -37,9 +38,9 @@
     <span style="font-weight: 600; font-size: 1.2rem; color: white;">Voltera</span>
   </div>
 
-  <!-- fake pagese -->
-  <div style="display: flex; gap: 1.5rem; align-items: center;">
-    <a href="#" style="color: white; text-decoration: none; font-size: 0.95rem; opacity: 0.8;">Voltera</a>
+  <!-- fake pages -->
+  <div class="nav-links" style="display: flex; gap: 1.5rem; align-items: center;">
+    <a href="#" style="color: white; text-decoration: none; font-size: 0.95rem; opacity: 0.8;">Desafio</a>
     <a href="#" style="color: white; text-decoration: none; font-size: 0.95rem; opacity: 0.8;">Front-End</a>
   </div>
 
@@ -112,14 +113,7 @@
     <!-- blur divs -->
     <div class="glow-div" style="transform: translate(-150%, -250%); box-shadow: 0 0 80px 40px mediumturquoise; background-color: blue">123</div>
     <div class="glow-div" style="transform: translate(100%, 250%); box-shadow: 0 0 100px 80px mediumturquoise; background-color: purple">123</div>
-    <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="blue" style="position: absolute; transform: translate(-60%, 50%) rotate(-15deg); z-index: -1; height: 20rem" x="0px" y="0px" viewBox="0 0 30 30">
-        <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
-    </svg>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="blue" style="position: absolute; transform: translate(60%, 50%) rotate(15deg); z-index: -1; height: 18rem" x="0px" y="0px" viewBox="0 0 50 50">
-            <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"></path>
-    </svg> -->
-
-            <svg xmlns="http://www.w3.org/2000/svg"
+    <svg xmlns="http://www.w3.org/2000/svg"
             fill="url(#blueGradient)"
             viewBox="0 0 30 30"
             class="glow-svg"
@@ -147,8 +141,8 @@
         <path d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"/>
         </svg>
 
-
-    <div style="display: flex; flex-direction: column; align-items: center; gap: 1rem">
+    <!-- hero -->
+    <div class="main-section" style="display: flex; flex-direction: column; align-items: center; gap: 1rem;">
         <div style="display: flex; flex-direction: column; align-items: center; gap: 2rem">
             <div style="background-color: white; padding: 0.2rem 1rem; border-radius: 5px; opacity: 1">
                 <div>
@@ -162,7 +156,7 @@
             </div>
             <div style="display: flex; flex-direction: column; gap: 1rem; justify-content: center; align-items: center;">
                 <h2 class="title" style="font-weight: 800">Olá, {data.data?.name ?? "visitante!"}</h2>
-                <p class="subtitle" style="font-weight: 300; text-align: center; width: 24rem; opacity: 50%">{"Digite um nome abaixo e receba uma estimativa precisa da idade atual dessa pessoa"}</p>
+                <p class="subtitle" style="font-weight: 300; text-align: center; width: 12rem; opacity: 50%">{"Digite um nome abaixo e receba uma estimativa precisa da idade atual dessa pessoa"}</p>
             </div>
 
             <div style="width: 100%; max-width: 24rem">
@@ -357,7 +351,7 @@
         </div>
     </div>
 
-    <div style="position: absolute; bottom: 5%; width: 80%; display: flex; justify-content: center; text-align: center; font-size: 0.9rem; font-weight: 200">
+    <div class="footer-text" style="width: 80%; display: flex; justify-content: center; text-align: center; font-size: 0.9rem; font-weight: 200;">
         Esse projeto foi feito por Cauã de Mattos Pereira, para o desafio da vaga de desenvolvedor front-end para a Voltera Energia.
     </div>
 
@@ -473,6 +467,32 @@
     background: rgba(255, 255, 255, 0.1);
     box-shadow: 0 0 0 3px rgba(0, 255, 255, 0.2);
   }
+
+  @media (max-width: 768px) {
+  .main-section {
+    padding-top: 20rem;
+  }
+
+  .footer-text {
+    position: relative;
+  }
+
+  .glow-svg {
+    transform: translate(35%, 150%)
+  }
+
+  .glow-svg-2 {
+    transform: translate(-90%, -90%)
+  }
+
+  .nav-links {
+    opacity: 0;
+    width: 0;
+    display: none;
+  }
+
+
+}
 
 
 </style>
